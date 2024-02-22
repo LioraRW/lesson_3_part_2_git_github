8882319b9d7b9f005ac85a7f79c4f9032f29ce89
Olkin <160742993+LioraRW@users.noreply.github.com>
2024-02-22 23:26:35 +0300

    Update Main.java

--- src/main/java/org/olkinskok/Main.java
+++ src/main/java/org/olkinskok/Main.java
@@ -1,7 +1,7 @@
 package org.olkinskok;
 
 public class Main {
-    private static String msg = Hello QA.Guru!!!!!!!!!!!!!!"
+    private static String msg = Hello QA.Guru!"
     public static void main(String[] args) {
         System.out.println(getMessage());
     }
